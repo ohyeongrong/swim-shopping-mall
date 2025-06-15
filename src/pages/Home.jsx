5e421f8eec2901179fa9fb7a6a57967f82d306fe
ohyeongrong <dhdudfhd920@gmail.com>
2025-06-15 22:38:25 +0900

    LikeSort, ProductSwiper 파일 오류 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import ProductSwiper from "../components/ProductSwiper";
+import ProductSwiper from "@/components/ProductSwiper";
 import useStore from "../store/useStore";
 
 function Home() {
