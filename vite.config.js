cdef0d2566592f098f58ab3ecf7ef2ca71bfa450
ohyeongrong <dhdudfhd920@gmail.com>
2025-07-21 23:55:48 +0900

    test: 프로젝트 배포용

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/swim-shopping-mall/',
   plugins: [react(), tailwindcss()],
   resolve: {
   alias: {
